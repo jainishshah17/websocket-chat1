4da82514a8a802608c6c18aa40efc74eaf4808e0
jainishshah17 <Jainish17#>
2014-08-23 13:37:05 -0700

    first commit

---
+++ target/scala-2.10/src_managed/main/controllers/routes.java
@@ -0,0 +1,19 @@
+// @SOURCE:C:/play-2.2.4/samples/java/websocket-chat/conf/routes
+// @HASH:5065bd6aacbadc046864531824f9912456751c1c
+// @DATE:Thu Aug 21 12:31:37 PDT 2014
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+public static class javascript {
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+}
+public static class ref {
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+}
+}
+          
